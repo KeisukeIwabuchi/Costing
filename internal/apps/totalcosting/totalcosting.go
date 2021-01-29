35d749d4279066da1e5648ae23361b4f6b180e03
Keisuke Iwabuchi <cola.ice.puyo@gmail.com>
2021-01-29 09:16:17 +0900

    update

--- internal/apps/totalcosting/totalcosting.go
+++ internal/apps/totalcosting/totalcosting.go
@@ -1,23 +1,5 @@
 package totalcosting
 
-// CalculationMethod is 月末仕掛品の計算方法
-type CalculationMethod int
-
-// 月末仕掛品の計算方法(先入先出法 or 平均法)
-const (
-	FIFO CalculationMethod = iota
-	AVG
-)
-
-// DefectiveProductMethod 正常仕損の計算方法
-type DefectiveProductMethod int
-
-// 正常仕損の計算方法(度外視法 or 非度外視法)
-const (
-	Neglecting DefectiveProductMethod = iota
-	NonNeglecting
-)
-
 // ElementType はBox図の要素の種別を表す
 type ElementType int
 
@@ -42,6 +24,38 @@ type Element struct {
 	Progress float64     // 加工進捗度
 }
 
+// IsLeftElement is ElementTypeがBox図左側の要素かを確認する
+// true: Left, false: Right
+func (e Element) IsLeftElement() bool {
+	leftElement := []ElementType{First, Input}
+
+	for _, v := range leftElement {
+		if e.Type == v {
+			return true
+		}
+	}
+
+	return false
+}
+
+// CalculationMethod is 月末仕掛品の計算方法
+type CalculationMethod int
+
+// 月末仕掛品の計算方法(先入先出法 or 平均法)
+const (
+	FIFO CalculationMethod = iota
+	AVG
+)
+
+// DefectiveProductMethod 正常仕損の計算方法
+type DefectiveProductMethod int
+
+// 正常仕損の計算方法(度外視法 or 非度外視法)
+const (
+	Neglecting DefectiveProductMethod = iota
+	NonNeglecting
+)
+
 // Cost is 仕掛品のBOX図
 type Cost struct {
 	InputOnAvg  bool
@@ -51,115 +65,6 @@ type Cost struct {
 	DMethod     DefectiveProductMethod
 	FirstCost   float64
 	InputCost   float64
-}
-
-// Box is 解く問題
-type Box struct {
-	Master           []Element
-	Costs            []Cost
-	ProductTotalCost float64
-	ProductAvgCost   float64
-	EOTMTotalCost    float64
-}
-
-// Run is culcurate answer
-func (b Box) Run() {
-	// 数量の計算
-	for _, cost := range b.Costs {
-		// 定点で投入
-		if !cost.InputOnAvg {
-			// 投入量の計算
-			cost.CalulateInputUnit(b.Master)
-		}
-
-		// 平均的に投入
-		if cost.InputOnAvg {
-			// 完成品換算量を計算
-			cost.CalulateConversionUnit(b.Master)
-		}
-	}
-
-	// 月末仕掛品原価の計算
-	for _, cost := range b.Costs {
-		// 先入先出法
-		if cost.CMethod == FIFO {
-			lastPrice := cost.GetPriceFIFO()
-			totalUnit := 0
-
-			// 完成品以外の平均単価を代入
-			for _, e := range cost.Elements {
-				if e.IsLeftElement() || e.Type == Output {
-					continue
-				}
-
-				e.Price = lastPrice
-				totalUnit += e.Unit
-			}
-
-			// 差額で完成品の平均単価を計算
-			for _, e := range cost.Elements {
-				if e.Type != Output {
-					continue
-				}
-
-				totalCost := cost.FirstCost + cost.InputCost
-				outputCost := totalCost - lastPrice*float64(totalUnit)
-				e.Price = outputCost / float64(e.Unit)
-			}
-		}
-
-		// 平均法
-		if cost.CMethod == AVG {
-			lastPrice := cost.GetPriceAVG()
-
-			for _, e := range cost.Elements {
-				if e.IsLeftElement() {
-					continue
-				}
-
-				e.Price = lastPrice
-			}
-		}
-	}
-
-	// 月末仕掛品原価の計算
-	b.CalculationEOFMCost()
-
-	// 完成品原価の計算
-	b.CalculationProductCost()
-}
-
-// CalculationEOFMCost is 月末仕掛品原価の計算
-func (b *Box) CalculationEOFMCost() {
-	b.EOTMTotalCost = 0.0
-
-	for _, c := range b.Costs {
-		for _, e := range c.Elements {
-			if e.Type == Last {
-				b.EOTMTotalCost += e.Price * float64(e.Unit)
-			}
-		}
-	}
-}
-
-// CalculationProductCost is 完成品原価の計算
-func (b *Box) CalculationProductCost() {
-	b.ProductTotalCost = 0.0
-
-	for _, c := range b.Costs {
-		for _, e := range c.Elements {
-			if e.Type == Output {
-				b.ProductTotalCost += e.Price * float64(e.Unit)
-			}
-		}
-	}
-
-	for _, e := range b.Master {
-		if e.Type == Output {
-			b.ProductAvgCost = b.ProductTotalCost / float64(e.Unit)
-			break
-		}
-	}
 }
 
 // CalulateInputUnit is 投入点と進捗度を比較して、進捗度が投入点以上なら投入
@@ -225,20 +130,6 @@ func (c Cost) CalulateConversionUnit(master []Element) {
 	}
 }
 
-// IsLeftElement is ElementTypeがBox図左側の要素かを確認する
-// true: Left, false: Right
-func (e Element) IsLeftElement() bool {
-	leftElement := []ElementType{First, Input}
-
-	for _, v := range leftElement {
-		if e.Type == v {
-			return true
-		}
-	}
-
-	return false
-}
-
 // GetPriceFIFO is 先入先出法での月末仕掛品平均単価を返す
 func (c Cost) GetPriceFIFO() float64 {
 	for _, e := range c.Elements {
@@ -261,4 +152,113 @@ func (c Cost) GetPriceAVG() float64 {
 	}
 
 	return (c.FirstCost + c.InputCost) / float64(totalUnit)
+}
+
+// Box is 解く問題
+type Box struct {
+	Master           []Element
+	Costs            []Cost
+	ProductTotalCost float64
+	ProductAvgCost   float64
+	EOTMTotalCost    float64
+}
+
+// CalculationEOFMCost is 月末仕掛品原価の計算
+func (b *Box) CalculationEOFMCost() {
+	b.EOTMTotalCost = 0.0
+
+	for _, c := range b.Costs {
+		for _, e := range c.Elements {
+			if e.Type == Last {
+				b.EOTMTotalCost += e.Price * float64(e.Unit)
+			}
+		}
+	}
+}
+
+// CalculationProductCost is 完成品原価の計算
+func (b *Box) CalculationProductCost() {
+	b.ProductTotalCost = 0.0
+
+	for _, c := range b.Costs {
+		for _, e := range c.Elements {
+			if e.Type == Output {
+				b.ProductTotalCost += e.Price * float64(e.Unit)
+			}
+		}
+	}
+
+	for _, e := range b.Master {
+		if e.Type == Output {
+			b.ProductAvgCost = b.ProductTotalCost / float64(e.Unit)
+			break
+		}
+	}
+}
+
+// Run is culcurate answer
+func (b *Box) Run() {
+	// 数量の計算
+	for _, cost := range b.Costs {
+		// 定点で投入
+		if !cost.InputOnAvg {
+			// 投入量の計算
+			cost.CalulateInputUnit(b.Master)
+		}
+
+		// 平均的に投入
+		if cost.InputOnAvg {
+			// 完成品換算量を計算
+			cost.CalulateConversionUnit(b.Master)
+		}
+	}
+
+	// 月末仕掛品原価の計算
+	for _, cost := range b.Costs {
+		// 先入先出法
+		if cost.CMethod == FIFO {
+			lastPrice := cost.GetPriceFIFO()
+			totalUnit := 0
+
+			// 完成品以外の平均単価を代入
+			for _, e := range cost.Elements {
+				if e.IsLeftElement() || e.Type == Output {
+					continue
+				}
+
+				e.Price = lastPrice
+				totalUnit += e.Unit
+			}
+
+			// 差額で完成品の平均単価を計算
+			for _, e := range cost.Elements {
+				if e.Type != Output {
+					continue
+				}
+
+				totalCost := cost.FirstCost + cost.InputCost
+				outputCost := totalCost - lastPrice*float64(totalUnit)
+				e.Price = outputCost / float64(e.Unit)
+			}
+		}
+
+		// 平均法
+		if cost.CMethod == AVG {
+			lastPrice := cost.GetPriceAVG()
+
+			for _, e := range cost.Elements {
+				if e.IsLeftElement() {
+					continue
+				}
+
+				e.Price = lastPrice
+			}
+		}
+	}
+
+	// 月末仕掛品原価の計算
+	b.CalculationEOFMCost()
+
+	// 完成品原価の計算
+	b.CalculationProductCost()
 }
--- internal/apps/totalcosting/totalcosting_test.go
+++ internal/apps/totalcosting/totalcosting_test.go
@@ -6,55 +6,27 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRun(t *testing.T) {
-	first := Element{
-		Type:     First,
-		Unit:     300,
-		Progress: 0.6,
-	}
-	input := Element{
-		Type: Input,
-		Unit: 1380,
-	}
-	output := Element{
-		Type: Output,
-		Unit: 1440,
-	}
-	last := Element{
-		Type:     Last,
-		Unit:     240,
-		Progress: 0.3,
+func TestIsLeftElement(t *testing.T) {
+	testCases := []struct {
+		E      Element
+		Result bool
+	}{
+		{Element{Type: First}, true},
+		{Element{Type: Input}, true},
+		{Element{Type: Output}, false},
+		{Element{Type: Last}, false},
+		{Element{Type: NormalDefect}, false},
+		{Element{Type: AbnormalDefect}, false},
+		{Element{Type: NormalImpairment}, false},
+		{Element{Type: AbnormalImpairment}, false},
 	}
 
-	var master []Element
-	master = append(master, first)
-	master = append(master, input)
-	master = append(master, output)
-	master = append(master, last)
-
-	var material, processing Cost
-	material.InputOnAvg = false
-	material.InputTiming = 0.0
-	material.CMethod = AVG
-	material.FirstCost = 206400
-	material.InputCost = 717600
-
-	processing.InputOnAvg = true
-	processing.FirstCost = 161640
-	processing.InputCost = 972360
-
-	var costs []Cost
-	costs = append(costs, material)
-	costs = append(costs, processing)
-
-	var box Box
-	box.Master = master
-	box.Costs = costs
-	box.Run()
-
-	actual := box.ProductAvgCost
-	expected := 1300.0
-	assert.Equal(t, expected, actual)
+	for _, testCase := range testCases {
+		result := testCase.E.IsLeftElement()
+		if result != testCase.Result {
+			t.Errorf("Invalid result. testCase:%#v, actual:%t", testCase, result)
+		}
+	}
 }
 
 func TestCalulateInputUnit(t *testing.T) {
@@ -141,29 +113,6 @@ func TestCalulateConversionUnit(t *testing.T) {
 	for i, e := range proccesing.Elements {
 		actual := e.Unit
 		assert.Equal(t, expected[i], actual)
-	}
-}
-
-func TestIsLeftElement(t *testing.T) {
-	testCases := []struct {
-		E      Element
-		Result bool
-	}{
-		{Element{Type: First}, true},
-		{Element{Type: Input}, true},
-		{Element{Type: Output}, false},
-		{Element{Type: Last}, false},
-		{Element{Type: NormalDefect}, false},
-		{Element{Type: AbnormalDefect}, false},
-		{Element{Type: NormalImpairment}, false},
-		{Element{Type: AbnormalImpairment}, false},
-	}
-
-	for _, testCase := range testCases {
-		result := testCase.E.IsLeftElement()
-		if result != testCase.Result {
-			t.Errorf("Invalid result. testCase:%#v, actual:%t", testCase, result)
-		}
 	}
 }
 
@@ -267,5 +216,54 @@ func TestCalculationProdutCost(t *testing.T) {
 
 	actual = box.ProductAvgCost
 	expected = 1300.0
+	assert.Equal(t, expected, actual)
+}
+
+func TestRun(t *testing.T) {
+	first := Element{
+		Type:     First,
+		Unit:     300,
+		Progress: 0.6,
+	}
+	input := Element{
+		Type: Input,
+		Unit: 1380,
+	}
+	output := Element{
+		Type: Output,
+		Unit: 1440,
+	}
+	last := Element{
+		Type:     Last,
+		Unit:     240,
+		Progress: 0.3,
+	}
+
+	var box Box
+
+	box.Master = append(box.Master, first)
+	box.Master = append(box.Master, input)
+	box.Master = append(box.Master, output)
+	box.Master = append(box.Master, last)
+
+	var material, processing Cost
+	material.InputOnAvg = false
+	material.InputTiming = 0.0
+	material.CMethod = AVG
+	material.FirstCost = 206400
+	material.InputCost = 717600
+
+	processing.InputOnAvg = true
+	processing.CMethod = AVG
+	processing.FirstCost = 161640
+	processing.InputCost = 972360
+
+	box.Costs = append(box.Costs, material)
+	box.Costs = append(box.Costs, processing)
+
+	box.Run()
+
+	actual := box.ProductAvgCost
+	expected := 1300.0
 	assert.Equal(t, expected, actual)
 }
